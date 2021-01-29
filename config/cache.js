d9d4605cb206eb5139f4b0d91e706305ac6d2002
Gal <gallib2@gmail.com>
2021-01-29 22:13:41 +0200

    Modify redis host

--- config/cache.js
+++ config/cache.js
@@ -7,7 +7,7 @@
 
 const REDIS_OPTIONS = {
     port: process.env.REDIS_PORT || 6379,
-    host: process.env.REDIS_HOST || 'localhost',
+    host: 'rediss://' + process.env.REDIS_HOST || 'localhost',
     password: process.env.REDIS_PASSWORD || 'secret'
 }
 
